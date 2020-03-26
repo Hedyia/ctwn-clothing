6407428d787b7197b5cb4781fe757d89759f6f16
Muhammed Hedyia <hedyia@hotmal.com>
2020-03-26 22:47:05 +0200

    finshing sign-in component and create custom components Custom button and Custom input

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import FormInput from "../form-input/form-input.component";
-
+import "./sign-in.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 class SignIn extends Component {
   state = {
     email: "",
@@ -38,6 +39,7 @@ class SignIn extends Component {
             required
             handleChange={this.handleChange}
           />
+          <CustomButton type="submit">Sign in</CustomButton>
         </form>
       </div>
     );
