ececc219617aa5c45414b201a1c5fa5f717fc8de
Muhammed Hedyia <hedyia@hotmal.com>
2020-04-04 00:47:47 +0200

    added sections to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cartReducer";
+import directoryReducer from "./directory/directoryReducer";
 export default combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
